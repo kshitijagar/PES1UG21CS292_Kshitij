f5145499afaf81f8ec306aceef8a6f0413147595
kshitijagar <kshitijagarwal2808@gmail.com>
2024-03-09 14:00:04 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,6 +6,6 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run\n";
-  cout << "I am trying to use ngrok with github webhooks\n";
+  cout << "I am trying to use ngrok with github Webhooks\n";
   return 0;
 }
